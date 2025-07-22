598c8743052a2ee0eecb06871f39546762e05d9c
Koveshan Munsami <codingstudent2021@gmail.com>
2025-07-22 17:58:34 +0200

    Implementing Astro Strapi team-members component

---
+++ src/types/image.d.ts
@@ -0,0 +1,42 @@
+// Define interfaces for the image formats
+interface ImageFormat {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: string | null;
+  size: number;
+  width: number;
+  height: number;
+  sizeInBytes: number;
+}
+
+interface ImageFormats {
+  small?: ImageFormat;
+  medium?: ImageFormat;
+  thumbnail?: ImageFormat;
+}
+
+// Define the Avatar structure
+export default interface ImageData {
+  id: number;
+  documentId: string;
+  name: string;
+  alternativeText: string | null;
+  caption: string | null;
+  width: number;
+  height: number;
+  formats: ImageFormats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string; // This is the main URL for the image from Strapi
+  previewUrl: string | null;
+  provider: string;
+  provider_metadata: any | null;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
